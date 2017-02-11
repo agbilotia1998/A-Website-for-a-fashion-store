bb0139f1172f9be3ef471daef5a84a79dfa37bba
agbilotia1998 <agbilotia1998@gmail.com>
2017-02-12 02:13:52 +0530

    Added Mail option

--- backend.js
+++ backend.js
@@ -4,16 +4,18 @@
 
 var express=require("express"),
     app=express(),
-    bodyParser=require("body-parser")
+    bodyParser=require("body-parser"),
     mongoose = require("mongoose");
-
+var nodemailer = require('nodemailer');
+var cred = require('./mail.js');
 mongoose.connect("mongodb://localhost/bb");
 
 var bbSchema=new mongoose.Schema({
     name:String,
     username:String,
     password:String,
-    email:String
+    email:String,
+    confirmation:String
 });
 
 
@@ -51,7 +53,7 @@ app.post("/loginned",function (req,res) {
 
     var user=req.body.username;
     var pass=req.body.password;
-    newacc.findOne({username:user, password:pass},function(err,bb){
+    newacc.findOne({username:user, password:pass,confirmation:"1"},function(err,bb){
 
 
                 if (!bb) {
@@ -64,6 +66,40 @@ app.post("/loginned",function (req,res) {
     })
 });
 
+
+
+app.get("/confirmation/username/:un",function(req,res){
+
+    var verify=(req.params.un);
+    newacc.findOne({username:(req.params.un)},function(err,bb){
+        if(bb) {
+           // newacc.findOne({username:(req.params.un)},bb.confirmation="1");
+            newacc.update({username:verify},{$set:{confirmation:"1"}},function (err, numUpdated) {
+                if (err) {
+                    console.log(err);
+                } else if (numUpdated) {
+                    console.log('Confirmation set to 1');
+                } else {
+                    console.log('No document found with defined "find" criteria!');
+                }
+                //Close connection
+                           });
+            res.render("loginned.ejs", {name: bb.name});
+        }
+
+        else{
+            res.send("USER NOT REGISTERED");
+        }
+        //console.log(newacc.confirmation);
+    })
+
+});
+
+
+
+
+
+
 app.post("/register",function(req,res)
     {
 
@@ -72,7 +108,8 @@ app.post("/register",function(req,res)
          name:req.body.name,
           username: req.body.username,
           password: req.body.password,
-          email: req.body.email
+          email: req.body.email,
+            confirmation:"0"
 
     };
 
@@ -83,8 +120,41 @@ app.post("/register",function(req,res)
                 newacc.findOne({email:details.email},function(err,bb){
 
                     if(!bb) {
-                        newacc.create(details);
-                        res.render("register.ejs", {name: details.name});
+
+
+                        //document.write('<scr'+'ipt type="text/javascript" src="mail.js" ></scr'+'ipt>');
+// create reusable transporter object using the default SMTP transport
+                 //        transporter = nodemailer.createTransport('smtps://agbilotia1998%40gmail.com:rajeshgupta@smtp.gmail.com');
+                            transporter=nodemailer.createTransport(cred.cred);
+// setup e-mail data with unicode symbols
+                        var mailOptions = {
+                            from: '"Banke Bihari Fashions <agbilotia1998@gmail.com>', // sender address
+                            to:details.email, // list of receivers
+                            subject: 'Registered ✔', // Subject line
+                            text: 'Registration successful 🐴', // plaintext body
+                            html:"<img src='http://deizi23.com/wp-content/uploads/2015/05/2015-05-21_14-50-28_%D1%87-.png'><br><br>"+
+
+                            '<b> Thanks for registering At Banke Bihari Fashions.... 🐴<br><br></b>' +
+                            'Proceed and shop at the Clothing Store..<br><br>'+
+                            ' So what are you waiting for , <br>' +
+                            'GO AHEAD AND SHOP!!!!!!!!!!!!!!<br><br>'+
+                               "<a href='http://127.0.0.1:3000/confirmation/username/" + details.username + "' target='_blank'> 'Click on the link to activate your account'</a><br>"+
+                            //"<a href='http:///localhost:3000/confirmation/username'> CLICK</a>"+
+
+                            '<br><i>HAPPY SHOPPING<br> <br></i>'+
+                            'FOR FURTHER QUERIES:<br>'+'CONTACT:<br>AYUSH GUPTA'// html body
+                        };
+
+// send mail with defined transport object
+                        transporter.sendMail(mailOptions, function(error, info){
+                            if(error){
+                                return console.log(error);
+                            }
+                            console.log('Message sent: ' + info.response);
+                            newacc.create(details);
+                            res.render("register.ejs", {name: details.name});
+
+                        });
                     }
 
                     else{
---
+++ public/frontend.js
@@ -0,0 +1,31 @@
+/**
+ * Created by AYUSH on 2/7/2017.
+ */
+
+
+document.addEventListener("DOMContentLoaded",function(){
+
+
+   $.ajax({
+
+       type:'GET',
+       dataType:'json',
+       url:'http://api.github.com/repos/fossiiita/opencodecollab/issues',
+       headers:{
+           'Accept':'application/vnd.github.v3+json'
+       },
+
+       success:function(data){
+
+         //var a= document.createElement('p');
+
+         $("#login").html(data[0].url);
+
+       }
+
+
+   })
+
+
+
+});
