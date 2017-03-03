ac56db6a3d06624035cc926394d5b30d6164f5e7
agbilotia1998 <agbilotia1998@gmail.com>
2017-03-03 16:39:55 +0530

    Removed plugin issue and added image slideshow

--- public/main.js
+++ public/main.js
@@ -35,3 +35,46 @@ $(document).ready(function () {
     });
 });
 
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 4000); // Change image every 2 seconds
+}
+
+var slideIndexx = 1;
+showDivs(slideIndexx);
+
+function plusDivs(n) {
+    showDivs(slideIndexx += n);
+}
+
+function currentDiv(n) {
+    showDivs(slideIndexx= n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > x.length) {slideIndexx = 1}
+    if (n < 1) {slideIndexx = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" w3-white", "");
+    }
+    x[slideIndexx-1].style.display = "block";
+    dots[slideIndexx-1].className += " w3-white";
+     //setTimeout(showDivs(n+1), 2000); // Change image every 2 seconds
+}
+
